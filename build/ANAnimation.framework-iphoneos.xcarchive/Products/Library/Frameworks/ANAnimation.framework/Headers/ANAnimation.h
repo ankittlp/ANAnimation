633c484e79724e03dcd4e8012394e121ac0dcbcc
Ankit Nigam <ankit.nigam@tothenew.com>
2023-07-03 16:30:06 +0530

    Build XCFramework

---
+++ build/ANAnimation.framework-iphoneos.xcarchive/Products/Library/Frameworks/ANAnimation.framework/Headers/ANAnimation.h
@@ -0,0 +1,18 @@
+//
+//  ANAnimation.h
+//  ANAnimation
+//
+//  Created by Ankit Nigam on 03/07/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ANAnimation.
+FOUNDATION_EXPORT double ANAnimationVersionNumber;
+
+//! Project version string for ANAnimation.
+FOUNDATION_EXPORT const unsigned char ANAnimationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ANAnimation/PublicHeader.h>
+
+
